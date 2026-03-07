6a268c4d8ad1bafc8a524e44bd78726c3ece8d5e
lakshettisapnil <lakshettysapnil@gmail.com>
2026-03-08 01:44:01 +0530

    Delete creating_construction.py

--- creating_construction.py
+++
@@ -1,7 +0,0 @@
-class Instagram:
-    def __init__(self,username,password):
-        self.username = username 
-        self.password = password 
-        print(f'Hey {username} well come to the Instagram')
-sapnil = Instagram("swapnil", 'Swapnil@123')
-praveen = Instagram("praveen", 'praveen@123')
